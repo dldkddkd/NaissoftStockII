2e913d596c1d257a979dd372f24ea1d014ffa031
kyungje cho <jkjk396@gmail.com>
2018-04-05 15:26:07 +0900

    change a part of startlogo() function 22line

--- Stock2/Arts.cpp
+++ Stock2/Arts.cpp
@@ -19,8 +19,14 @@ void startLogo()
 	gotoxy(1, 2), printf("유의 사항 및 저작권 안내");
 	Sleep(300);
 	gotoxy(1, 6);
-	printf(" 이 게임은 실제 주식 투자와 상당히 다릅니다. 실제로 주식 투자를 할 때에 주의하십시오.\n  게임 내의 회사는 실존 회사를 묘사하지 않으며, 그 내용은 모두 허구입니다.\n ");
-	printf(" 소스 코드를 상업적으로 이용하는 것은 금지되어 있습니다.\n  피드백 및 문의는 moon44432.blog.me에서 하실 수 있습니다.\n  Stock!과 Stock II의 저작권은 Naissoft가 보유하고 있습니다.\n\n\n ⓒ 2013~2018 Naissoft. All rights reserved.\n\n 시작하려면 아무 키나 누르십시오.");
+	printf( "%s%s%s%s%s%s%s",
+			"이 게임은 실제 주식 투자와 상당히 다릅니다. 실제로 주식 투자를 할 때에 주의하십시오.\n",
+			" 게임 내의 회사는 실존 회사를 묘사하지 않으며, 그 내용은 모두 허구입니다.\n",
+			" 소스 코드를 상업적으로 이용하는 것은 금지되어 있습니다.\n",
+			" 피드백 및 문의는 moon44432.blog.me에서 하실 수 있습니다.\n",
+			" Stock!과 Stock II의 저작권은 Naissoft가 보유하고 있습니다.\n\n\n",
+			" ⓒ 2013~2018 Naissoft. All rights reserved.\n\n",
+			" 시작하려면 아무 키나 누르십시오.");
 	getch();
 	textcolor(7);
 	gotoxy(1, 2), printf("유의 사항 및 저작권 안내");
